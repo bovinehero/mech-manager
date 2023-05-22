30a4a22ef7d4017aaeb96d564731469e8c01e274
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-22 17:06:24 +0100

    adding django_heroku as per docs

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,6 @@ STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
+
