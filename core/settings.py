2000cb84794f974d9a031a305a7aeb8505a6a10f
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-22 17:54:53 +0100

    config typo fix2

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-SITE_ROOT = os.path.abspath(os.path.dirname(file))
+SITE_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
