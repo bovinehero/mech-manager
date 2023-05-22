3f50d97b8c4211dad20c2208d446dd0f414fa7f7
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-22 16:29:48 +0100

    adding models and corresponding views

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import itertools
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+TECH = ((0, 'Inner Sphere'), (1, 'Clan'), (2, 'Prototype'))
+ROLE = ((0, 'Ambusher'), (1, 'Brawler'), (2, 'Juggernaut'), (3, 'Missle Boat'), (4, 'Scout'), (5, "Skirmisher"), (6, 'Sniper'), (7, 'Striker'))
+CLASSIFICATION = ((0, 'Light'), (1, 'Medium'), (2, 'Heavy'), (3, 'Assault'))
+WEIGHTS = ((0, '20 Ton'), (1, '25 Ton'), (2, '30 Ton'), (3, '35 Ton'), (4, '40 Ton'), (5, '45 Ton'), (6, '50 Ton'), (7, '55 Ton'), (8, '60 Ton'), (9, '65 Ton'), (10, '70 Ton'), (11, '75 Ton'), (12, '80 Ton'), (13, '85 Ton'), (14, '90 Ton'), (15, '95 Ton'), (16, '100 Ton'))
+STATUS = ((0, "Not Available"), (1, "Available"))
+
+   
+class Mech(models.Model):
+
+    name = models.CharField(max_length=50, unique=True)
+    category = models.IntegerField(choices=CLASSIFICATION, default=0)
+    weight = models.IntegerField(choices=WEIGHTS, default=0)
+    tech_level = models.IntegerField(choices=TECH, default=0)
+    role = models.IntegerField(choices=ROLE, default=0)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    record_sheet = models.CharField(max_length=50, default='custom')
+    battle_value = models.IntegerField(default=9999)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def save(self, *args, **kwargs):
+        value = self.name
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class MechList(generic.ListView):
+    model = Mech
+    queryset = Mech.objects.all().order_by('name')
+    template_name = 'mechs.html'
+    paginate_by = 10
+
+class MechDetail(generic.ListView):
+    
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Mech.objects.all()
+        mech = get_object_or_404(queryset, slug=slug)
+        return render(
+            request,
+            "mech_detail.html",
+            {
+                "mech": mech
+            },
+        )
+ 
+class CreateMechView(generic.CreateView):
+    model = Mech
+    form_class = CreateMechForm
+    template_name = 'mechs_form.html'    
+    success_url = reverse_lazy('mechs')
+
+    def form_valid(self, form):
+        messages.success(self.request, "The Mech was successfully created.")
+        return super(CreateMechView,self).form_valid(form)
+
+class UpdateMechView(UpdateView):
+    model = Mech
+    form_class = UpdateMechForm
+    template_name = 'mechs_form.html'    
+    success_url = reverse_lazy('mechs')
+
+    def form_valid(self, form):
+        messages.success(self.request, "The Mech was successfully updated.")
+        return super(UpdateMechView,self).form_valid(form)
+
+class DeleteMechView(DeleteView):
+    model = Mech
+    context_object_name = 'mech'
+    success_url = reverse_lazy('mechs')
+    template_name = 'mech_delete.html'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "The Mech was successfully deleted.")
+        return super(DeleteMechView, self).delete(request, *args, **kwargs)
+    
+   
+def toggle_mech_status(request, slug):
+    mech = get_object_or_404(Mech, slug=slug)
+    if mech.status == 0:
+        mech.status = 1
+    else:
+        mech.status = 0
+    mech.save()
+    return redirect('mechs')
