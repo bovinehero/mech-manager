4df34334131ae4697accb7b501dce17ac2abcccb
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-22 20:49:00 +0100

    adding models to admin app

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,9 @@ import dj_database_url
 from django.contrib.messages import constants as messages
 from pathlib import Path
 
+if os.path.isfile("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Mech
 
-# Register your models here.
+@admin.register(Mech)
+class MechAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'weight',
+        'tech_level',
+        'role',
+        'record_sheet',
+        'battle_value',
+        'status'
+        ) 
+    search_fields = ['type', 'tech_level']
+    list_filter = ('tech_level', )
+    actions = ['approve_mech', 'revoke_mech']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def approve_mech(self, request, queryset):
+        queryset.update(status=True)
+    
+    def revoke_mech(self, request, queryset):
+        queryset.update(status=False)
