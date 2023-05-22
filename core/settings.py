1fde9014fb071ddd4c697eefbf9544a0ea0558b3
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-22 17:21:28 +0100

    removing heroku locals

--- core/settings.py
+++ core/settings.py
@@ -130,5 +130,5 @@ STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
