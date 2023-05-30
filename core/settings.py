d70e224031d46b75127b9e5c57384f4e695fbdfa
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-05-30 21:52:48 +0100

    flipp db back to prod mode for deployment

--- core/settings.py
+++ core/settings.py
@@ -102,13 +102,6 @@ DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
